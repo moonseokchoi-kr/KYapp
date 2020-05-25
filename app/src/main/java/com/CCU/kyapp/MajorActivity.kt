7901c45ae9be9e22aaa869256bf05372e1a13b22
MoonSeok Choi <anstjr0613@gmail.com>
2020-05-25 21:09:02 +0900

    make MajorActivity and add Youtube lib this lib use for thumbnail viewer

---
+++ app/src/main/java/com/CCU/kyapp/MajorActivity.kt
@@ -0,0 +1,12 @@
+package com.CCU.kyapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MajorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_major)
+    }
+}
