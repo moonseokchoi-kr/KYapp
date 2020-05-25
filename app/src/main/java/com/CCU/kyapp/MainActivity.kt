675fc1ab635a00e631cc7fe1da164abf74ca7bc6
MoonSeok Choi <anstjr0613@gmail.com>
2020-05-26 00:08:06 +0900

    make Intro Text and make Youtube View

--- app/src/main/java/com/CCU/kyapp/MainActivity.kt
+++ app/src/main/java/com/CCU/kyapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.CCU.kyapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.prnd.YouTubePlayerView
+import kotlinx.android.synthetic.main.activity_major.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_major)
+        YouTubePlayerView_major.play("UTx1igNpTpk")
     }
 }
--- app/src/main/java/com/CCU/kyapp/MajorActivity.kt
+++ app/src/main/java/com/CCU/kyapp/MajorActivity.kt
@@ -1,10 +1,21 @@
 package com.CCU.kyapp
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kr.co.prnd.YouTubePlayerView
+import kotlinx.android.synthetic.main.activity_major.*
 
 class MajorActivity : AppCompatActivity() {
 
+
+    var url = "https://youtu.be/UTx1igNpTpk"
+    val youtube_key = "AIzaSyDrI2s15niXWy3bZlE_Ms16h9oxG1oJt3E"
+    val REQ_START_STANDALONE_PLAYER = 101
+    val REQ_RESOLVE_SERVICE_MISSING = 2
+    val RECOVERY_DIALOG_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_major)
