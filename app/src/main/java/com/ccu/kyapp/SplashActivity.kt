a2bfc5b08b4928d6327e0b58ae517cc1a64c85d7
moonseokchoi-kr <moonseoktech@gmail.com>
2020-06-29 16:16:09 +0900

    add comment SplashActivity.kt

--- app/src/main/java/com/ccu/kyapp/SplashActivity.kt
+++ app/src/main/java/com/ccu/kyapp/SplashActivity.kt
@@ -5,18 +5,33 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * display splash view
+ *
+ * @author MoonSeok Choi
+ * @version 1.0 create splash view
+ * @since 2020.06.25
+ */
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        var hd : Handler = Handler()
-        hd.postDelayed(Runnable { var intent = Intent(this,
+        /*
+        active splash view
+         */
+        val hd : Handler = Handler()
+        hd.postDelayed(Runnable { val intent = Intent(this,
             MainActivity::class.java)
             startActivity(intent)
         this.finish()},3000)
     }
 
+    /**
+     * don't active back button
+     *
+     * @param None
+     * @return None
+     */
     override fun onBackPressed() {
     }
 }
